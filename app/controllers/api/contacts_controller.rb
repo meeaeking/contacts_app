83dbd1b8242568e903d5ab20f876f2a2505b71f5
meeae <meeaeking@gmail.com>
2019-07-01 20:47:57 -0500

    add update method

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -11,12 +11,23 @@ class Api::ContactsController < ApplicationController
       phone_number: params[:phone_number],
       email: params[:email]
       )
+    @contact.save
     render 'show.json.jb'
   end
 
   def show
     @contact = Contact.find_by(id: params[:id])
 
+    render 'show.json.jb'
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.phone_number = params[:phone_number]
+    @contact.email = params[:email]
+    @contact.save
     render 'show.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     get "/contacts" => "contacts#index"
     post "/contacts" => 'contacts#create'
     get "/contacts/:id" => 'contacts#show'
+    patch "/contacts/:id" => 'contacts#update'
   end
 end
